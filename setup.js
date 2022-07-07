6f2b71f1d9ea9e65d617b5df859461f76403f762
aliensanderdiaz <aliensanderdiaz@gmail.com>
2022-07-07 00:41:15 -0500

    first commit

---
+++ setup.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')
+
+async function setup() {
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    // dialectOptions: {
+    //   bigNumberStrings: true
+    // },
+    logging: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError(err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
